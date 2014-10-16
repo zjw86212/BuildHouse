67fa35b0c66e7e15ce88079b4cdd889a4595e1f0
zjw86212 <zjw86212@gmail.com>
2014-10-16 09:34:15 +0800

    Revert "start"
    This reverts commit 13b75aed2955ba37a89945c632f0473d032de0e2.

--- main.js
+++
@@ -1,9 +0,0 @@
-cc.game.onStart = function(){
-    cc.view.setDesignResolutionSize(300, 480, cc.ResolutionPolicy.SHOW_ALL);
-	cc.view.resizeWithBrowserSize(true);
-    //load resources
-    cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new MenuScene());
-    }, this);
-};
-cc.game.run();
